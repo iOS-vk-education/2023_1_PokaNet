a872938fee2456acce0acf179a2e9c33e393cfe3
primmapola <dongrigory29@gmail.com>
2023-11-21 18:29:48 +0300

    Precommit

--- PokaNetFilms/PokaNetFilms/ProfileScene/View/ProfileViewController.swift
+++ PokaNetFilms/PokaNetFilms/ProfileScene/View/ProfileViewController.swift
@@ -9,80 +9,85 @@ import Foundation
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    
+
     private let output: ProfileViewOutput
     private var model: ProfileViewModel?
-    
+
     //MARK: - UI elements
     private let headerView = ProfileHeaderView()
     private let filmsView = ProfileFilmsView()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         output.didLoadView()
         setupUI()
     }
-    
+
     init(output: ProfileViewOutput) {
         self.output = output
-        
+
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
-//MARK: - Settings for UI
-private extension ProfileViewController {
+//MARK: -
+extension ProfileViewController {
     func setupUI() {
-        view.backgroundColor = .systemGray4
-        setupViewLayer()
-        setupFilmsLayer()
+        setupProfileCollectionView()
     }
     
-    func setupViewLayer() {
-        view.addSubview(headerView)
+    func setupProfileCollectionView() {
         
-        headerView.layer.cornerRadius = 25
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 60, height: 60)
         
-        makeConstraintsViewLayer()
-    }
-    
-    func makeConstraintsViewLayer() {
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+        let profileCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        view.addSubview(profileCollectionView)
+        
+        profileCollectionView.delegate = self
+        profileCollectionView.dataSource = self
+        profileCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        profileCollectionView.backgroundColor = .red
+        
+        profileCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.topAnchor),
-            headerView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            headerView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            headerView.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -110)
-        ])
-    }
-    
-    func setupFilmsLayer() {
-        view.addSubview(filmsView)
-        
-        filmsView.layer.cornerRadius = 25
-        filmsView.backgroundColor = .white
-        
-        makeConstraintsFilmsLayer()
-    }
-    
-    func makeConstraintsFilmsLayer() {
-        filmsView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            filmsView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 16),
-            filmsView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
-            filmsView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
-            filmsView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+            profileCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            profileCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            profileCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            profileCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
+
+extension ProfileViewController: UICollectionViewDelegate {
+    
+}
+
+extension ProfileViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ProfileFavouriteFilmCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+    
+    
+}
+
 
 //MARK: - ProfileViewInput
 extension ProfileViewController: ProfileViewInput {
@@ -91,7 +96,7 @@ extension ProfileViewController: ProfileViewInput {
             headerView.emailLabel.text = newModel.profile.email
             headerView.avatar.image = newModel.profile.avatar
             headerView.userNameLabel.text = newModel.profile.userName
-            
+
             filmsView.configure(newModel)
         } else {
             print("!!!")
