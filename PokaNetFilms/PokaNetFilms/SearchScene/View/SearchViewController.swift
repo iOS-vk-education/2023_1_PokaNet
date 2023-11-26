285401b4949360abe5c3107450e2583b87cab7e5
primmapola <dongrigory29@gmail.com>
2023-11-26 15:13:13 +0300

    Add title

--- PokaNetFilms/PokaNetFilms/SearchScene/View/SearchViewController.swift
+++ PokaNetFilms/PokaNetFilms/SearchScene/View/SearchViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 final class SearchViewController: UIViewController {
     
     let output: SearchViewOutput
+    
+    var titleLable: UILabel!
     var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        setupSearchBar()
+        setupUI()
     }
     
     init(output: SearchViewOutput) {
@@ -32,6 +33,12 @@ final class SearchViewController: UIViewController {
 
 //MARK: - SetupUI
 private extension SearchViewController {
+    func setupUI() {
+        view.backgroundColor = .white
+        setupTitle()
+        setupSearchBar()
+    }
+    
     func setupSearchBar() {
         searchBar = UISearchBar()
         searchBar.translatesAutoresizingMaskIntoConstraints = false
@@ -40,9 +47,25 @@ private extension SearchViewController {
         view.addSubview(searchBar)
 
         NSLayoutConstraint.activate([
-            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.topAnchor.constraint(equalTo: titleLable.bottomAnchor),
             searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    func setupTitle() {
+        titleLable = UILabel()
+        titleLable.translatesAutoresizingMaskIntoConstraints = false
+        titleLable.text = "Поиск"
+        titleLable.textColor = .systemBlue
+        titleLable.font = UIFont.systemFont(ofSize: 40)
+        view.addSubview(titleLable)
+        
+        NSLayoutConstraint.activate([
+            titleLable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleLable.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            titleLable.rightAnchor.constraint(equalTo: view.rightAnchor),
+            titleLable.heightAnchor.constraint(equalToConstant: 60)
         ])
     }
 }
