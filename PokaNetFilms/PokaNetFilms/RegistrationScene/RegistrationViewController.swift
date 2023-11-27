4fc40c4e93d8227a8880ec09e7c77fe8433d30ca
mamillerr <mamillerr17@gmail.com>
2023-11-27 14:07:13 +0300

    Added image & label

--- PokaNetFilms/PokaNetFilms/RegistrationScene/RegistrationViewController.swift
+++ PokaNetFilms/PokaNetFilms/RegistrationScene/RegistrationViewController.swift
@@ -12,18 +12,60 @@ final class RegistrationViewController: UIViewController {
     
     private let output: RegistrationViewOutput
     
+    private let imagePK = UIImageView()
+    private let labelReg = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#function)
+        view.backgroundColor = .white
+        
+        setupImagePK()
+        setupLabelReg()
+    }
+    
     init(output: RegistrationViewOutput) {
         self.output = output
-        
         super.init(nibName: nil, bundle: nil)
     }
     
-    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupImagePK() {
+        self.view.addSubview(imagePK)
+        imagePK.clipsToBounds = true
+        imagePK.contentMode = .scaleAspectFill
+        imagePK.image = UIImage(named: "PNImage")
+        imagePK.sizeToFit()
+//        imagePK.frame = CGRect(x: 165, y: 140, width: 60, height: 60)
+        imagePK.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imagePK.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
+            imagePK.widthAnchor.constraint(equalToConstant: 60),
+            imagePK.heightAnchor.constraint(equalToConstant: 60),
+            imagePK.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    func setupLabelReg() {
+        view.addSubview(labelReg)
+        labelReg.text = "Регистрация"
+        labelReg.font = UIFont.systemFont(ofSize: 30)
+        labelReg.textAlignment = .center
+        labelReg.translatesAutoresizingMaskIntoConstraints = false // для активации верстки кодом
+        NSLayoutConstraint.activate([
+            labelReg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            labelReg.leftAnchor.constraint(equalTo: view.leftAnchor),
+            labelReg.rightAnchor.constraint(equalTo: view.rightAnchor),
+            labelReg.bottomAnchor.constraint(equalTo: view.topAnchor, constant: 400)
+        ])
     }
 }
 
 extension RegistrationViewController: RegistrationViewInput {
     
 }
+
